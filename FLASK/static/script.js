03cd4816752a71609bb81e89e3b43077f0d9cc9f
JASH1558 <jashwanthdonthula@gmail.com>
2026-05-26 18:55:29 +0530

    Update script.js

--- FLASK/static/script.js
+++ FLASK/static/script.js
@@ -124,7 +124,7 @@ function sendToNetwork() {
     const pixels = Array.from(imageData.data);
 
     // Send to backend
-    fetch("http://127.0.0.1:5000/predict", {
+    fetch("/predict", {
 
         method: "POST",
 
