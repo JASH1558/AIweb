b1d8e59f44b37dc97da5d9bd6650505af975496d
JASH1558 <jashwanthdonthula@gmail.com>
2026-05-29 20:18:52 +0530

    Implement convolutional neural network for MNIST
    Replaced the original neural network with a convolutional neural network for improved performance on MNIST dataset.

--- FLASK/main.py
+++ FLASK/main.py
@@ -1,58 +1,38 @@
 import torch 
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+
 
 device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-print("Using device:", device)
-train_data=datasets.MNIST(root="./data",train=True,download=True,transform=transforms.ToTensor())
-test_data=datasets.MNIST(root="./data",train=False,download=True,transform=transforms.ToTensor())
-train_loader=DataLoader(train_data,batch_size=6000,shuffle=True)
-test_loader=DataLoader(test_data,batch_size=1000,shuffle=False)
 
-class neural_network(nn.Module):
+class conv(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten=nn.Flatten()
-        self.layers=nn.Sequential(
-            nn.Linear(784,384),
-            nn.ReLU(),
-            nn.Linear(384,64),
-            nn.ReLU(),
-            nn.Linear(64,10)
-        )
-
+        self.conv1=nn.Conv2d(1,8,3,1,1)
+        self.conv2=nn.Conv2d(8,16,3,1,1)
+        self.fc1=nn.Linear(16*7*7,128)
+        self.fc2=nn.Linear(128,64)
+        self.fc3=nn.Linear(64,10)
+        self.pool=nn.MaxPool2d(2,2)
+        self.relu=nn.ReLU()
     def forward(self,x):
-            x=self.flatten(x)
-            x = self.layers(x)
-            return x
+        x=self.conv1(x)
+        x=self.relu(x)
+        x=self.pool(x)
+        x=self.conv2(x)
+        x=self.relu(x)
+        x=self.pool(x)
+        x=torch.flatten(x,1)
+        x=self.fc1(x)
+        x=self.relu(x)
+        x=self.fc2(x)
+        x=self.relu(x)
+        x=self.fc3(x)
+        return x
 
-model=neural_network().to(device)
-criterion=nn.CrossEntropyLoss()
-optimizer=optim.Adam(model.parameters(),lr=0.01)
-num_epochs=15
-for epoch in range(num_epochs):
-    model.train()
-    for images,labels in train_loader:
-        images,labels=images.to(device),labels.to(device)
-        outputs=model(images)
-        loss=criterion(outputs,labels)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
 
-model.eval()
-with torch.no_grad():
-    correct=0
-    total=0
-    for images,labels in test_loader:
-        images,labels=images.to(device),labels.to(device)
-        outputs=model(images)
-        _,predicted=torch.max(outputs.data,1)
-        total+=labels.size(0)
-        correct+=(predicted==labels).sum().item()
-    print(f"Test Accuracy: {100*correct/total:.2f}%")
+
+
+model=conv().to(device)
 
 torch.save(model.state_dict(),"mnist_model.pth")
