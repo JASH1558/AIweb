4f581d52bdbaa2cc26eaa771a28c05ce16ec27e5
JASH1558 <jashwanthdonthula@gmail.com>
2026-05-31 19:49:11 +0530

    Update main.py

--- FLASK/main.py
+++ FLASK/main.py
@@ -15,18 +15,34 @@ class conv(nn.Module):
         self.pool=nn.MaxPool2d(2,2)
         self.relu=nn.ReLU()
     def forward(self,x):
+        print("A", flush=True)
+    
         x=self.conv1(x)
+        print("B", flush=True)
+    
         x=self.relu(x)
         x=self.pool(x)
+        print("C", flush=True)
+    
         x=self.conv2(x)
+        print("D", flush=True)
+    
         x=self.relu(x)
         x=self.pool(x)
+        print("E", flush=True)
+    
         x=torch.flatten(x,1)
+        print("F", flush=True)
+    
         x=self.fc1(x)
-        x=self.relu(x)
+        print("G", flush=True)
+    
         x=self.fc2(x)
-        x=self.relu(x)
+        print("H", flush=True)
+    
         x=self.fc3(x)
+        print("I", flush=True)
+
         return x
     
 
